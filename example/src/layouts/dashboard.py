8df25bcdd54ec77b878f70d0a608c0fb5ce2b69e
ponsoc <36486184+ponsoc@users.noreply.github.com>
2025-03-24 09:13:50 +0100

    Update dashboard.py

--- example/src/layouts/dashboard.py
+++ example/src/layouts/dashboard.py
@@ -1,13 +1,19 @@
 import streamlit as st
+from components import MetricComponent
 from shared import ComponentIDs
-from ststeroids import Flow, Layout
+from ststeroids import Layout
 
 
 class DashboardLayout(Layout):
     def __init__(self):
-        st.empty()
+       self.total_movies = MetricComponent(ComponentIDs.total_movies,"Total movies")
+       self.avg_rating = MetricComponent(ComponentIDs.avg_rating, "Avg. Rating")
         
 
     def render(self):
-        st.write("Nog niet ingelogd. Gebruik het menu of ververs de pagina.")
-        
+        left, right = st.columns([1,1])
+        with left:
+            self.total_movies.render()
+        with right:
+            self.avg_rating.render()
+
