fd33c82f9426246984b9e7f6fda9357aa841b268
ponsoc <36486184+ponsoc@users.noreply.github.com>
2025-05-19 11:09:13 +0200

    Update __init__.py

--- example/src/components/__init__.py
+++ example/src/components/__init__.py
@@ -2,6 +2,7 @@ from .login_dialog import LoginDialogComponent
 from .sidebar import SidebarComponent
 from .data_viewer import DataViewerComponent
 from .metric import MetricComponent
+from .live_metric import LiveMetricComponent
 
 
-__all__ = [LoginDialogComponent, SidebarComponent, DataViewerComponent, MetricComponent]
+__all__ = [LoginDialogComponent, SidebarComponent, DataViewerComponent, MetricComponent, LiveMetricComponent]
